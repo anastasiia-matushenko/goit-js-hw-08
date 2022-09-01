d91071fd34190e9cd5ae233615e90ebed7b80550
anastasiia-matushenko <muzaam07@gmail.com>
2022-09-01 22:17:47 +0300

    added import simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// import simpleLightbox  from 'simplelightbox';
+// import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const imagesGallery = galleryItems.reduce((acc, {preview, original, description}) =>
+  acc +
+    `<a class="gallery__item" href="${original}">
+         <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+    </a>`
+  , "");
+
+gallery.insertAdjacentHTML("beforeend", imagesGallery);
+
+const galleryYes = new SimpleLightbox('.gallery a', {captionsData: 'alt',
+    captionDelay: 250});
